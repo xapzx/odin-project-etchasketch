25f8cbcc59acfa3615d4c6da71a6e2d112c917ac
Andy Phan <a1785560@student.adelaide.edu.au>
2022-11-12 18:19:03 +1030

    Added functionality to allow drawing on grid by hovering mouse over

--- scripts.js
+++ scripts.js
@@ -1,17 +1,23 @@
 const grid = document.querySelector('.grid-container');
 
-const grid_size = 16
-for (let row = 0; row < grid_size; row++) {
-    const row = document.createElement('div');
-    row.classList.add("grid-row");
+const grid_size = 16;
+// Create grid of selected size on webpage
+function createGrid(size) {
+    for (let row = 0; row < grid_size; row++) {
+        const row = document.createElement('div');
+        row.classList.add("grid-row");
 
-    for(let col = 0; col < grid_size; col++) {
-        const col = document.createElement('div');
-        col.classList.add("grid-col");
-        col.style.width = 500 / grid_size + "px";
-        col.style.height = 500 / grid_size + "px";
-        row.appendChild(col);
+        for(let col = 0; col < grid_size; col++) {
+            const col = document.createElement('div');
+            col.classList.add("grid-col");
+            col.style.width = 500 / grid_size + "px";
+            col.style.height = 500 / grid_size + "px";
+            col.addEventListener('mouseover', (event) => {event.target.classList.add("square-clicked")});
+            row.appendChild(col);
+        }
+
+        grid.appendChild(row);
     }
-
-    grid.appendChild(row);
 }
+
+createGrid(grid_size);

3ec7e6330ed83d35ebba71d4cc9f714a6ec634d3
Andy Phan <a1785560@student.adelaide.edu.au>
2022-11-12 19:02:36 +1030

    Added a slider with input box. Slider changes input of value and vice versa

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ const grid = document.querySelector('.grid-container');
 
 const grid_size = 16;
 // Create grid of selected size on webpage
+// Adds event listener to each square to change color when mouse over
 function createGrid(size) {
     for (let row = 0; row < grid_size; row++) {
         const row = document.createElement('div');
@@ -19,5 +20,25 @@ function createGrid(size) {
         grid.appendChild(row);
     }
 }
+
+const gridInputBox = document.querySelector('#gridInput');
+const gridSlider = document.querySelector('#gridRange');
+
+gridInputBox.addEventListener('input', function() {
+    let inputVal = this.value;
+
+    // Limit input to 1 - 100 (inclusive)
+    if(this.value < 1) {
+        inputVal = 1;
+    } else if(this.value > 100) {
+        inputVal = 100;
+    }
+
+    gridSlider.value = inputVal;
+});
+
+gridSlider.addEventListener('input', function() {
+    gridInputBox.value = this.value
+});
 
 createGrid(grid_size);

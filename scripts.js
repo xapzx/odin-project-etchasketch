628759a99c5a32c3affd4ff49265f14d1d80fbe8
Andy <andy@test.com>
2022-11-12 22:42:10 +1030

    Implemented a button to create the grid based on slider/inputted value

--- scripts.js
+++ scripts.js
@@ -1,18 +1,23 @@
 const grid = document.querySelector('.grid-container');
+const gridInputBox = document.querySelector('#gridInput');
+const gridSlider = document.querySelector('#gridRange');
+const gridCreateButton = document.querySelector('.gridCreate');
 
-const grid_size = 16;
 // Create grid of selected size on webpage
 // Adds event listener to each square to change color when mouse over
-function createGrid(size) {
-    for (let row = 0; row < grid_size; row++) {
+function createGrid() {
+    grid.innerHTML = "";
+    gridSize = gridSlider.value;
+
+    for (let row = 0; row < gridSize; row++) {
         const row = document.createElement('div');
         row.classList.add("grid-row");
 
-        for(let col = 0; col < grid_size; col++) {
+        for(let col = 0; col < gridSize; col++) {
             const col = document.createElement('div');
             col.classList.add("grid-col");
-            col.style.width = 500 / grid_size + "px";
-            col.style.height = 500 / grid_size + "px";
+            col.style.width = 500 / gridSize + "px";
+            col.style.height = 500 / gridSize + "px";
             col.addEventListener('mouseover', (event) => {event.target.classList.add("square-clicked")});
             row.appendChild(col);
         }
@@ -21,24 +26,27 @@ function createGrid(size) {
     }
 }
 
-const gridInputBox = document.querySelector('#gridInput');
-const gridSlider = document.querySelector('#gridRange');
-
+// Updates slider to represent inputted value in text box
 gridInputBox.addEventListener('input', function() {
     let inputVal = this.value;
 
     // Limit input to 1 - 100 (inclusive)
-    if(this.value < 1) {
+    if(inputVal < 1) {
         inputVal = 1;
-    } else if(this.value > 100) {
+    } else if(inputVal > 100) {
         inputVal = 100;
     }
 
     gridSlider.value = inputVal;
 });
 
+// Updates input box with slider changes
 gridSlider.addEventListener('input', function() {
     gridInputBox.value = this.value
 });
 
-createGrid(grid_size);
+// Creates new grid with inputted value from slider or input box
+gridCreateButton.addEventListener('click', createGrid);
+
+// Initialise Grid
+createGrid();
